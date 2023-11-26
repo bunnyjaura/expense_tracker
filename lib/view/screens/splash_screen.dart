91515e0ec594ff79b9d1a2000f3dcbcd686fe692
bunnyjaura <ysoni419@gmail.com>
2023-11-26 12:06:53 +0530

    a

--- lib/view/screens/splash_screen.dart
+++ lib/view/screens/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:expanse_tracker/controller/expense_provider.dart';
-import 'package:expanse_tracker/controller/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
