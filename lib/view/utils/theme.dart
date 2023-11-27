d59ed4712d563f9b5f62aa8a2670dee8c1be39b1
bunnyjaura <ysoni419@gmail.com>
2023-11-27 10:20:40 +0530

    font

--- lib/view/utils/theme.dart
+++ lib/view/utils/theme.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 final ThemeData lightTheme = ThemeData.light(useMaterial3: true).copyWith(
   primaryColor: Colors.cyan[200],
-  textTheme: GoogleFonts.poppinsTextTheme(),
+   textTheme: Typography.material2018(platform: TargetPlatform.android).white.apply(
+    fontFamily: 'Poppins',
+  ),
   canvasColor: Colors.black,
   cardColor: Colors.grey[200],
   highlightColor: const Color.fromARGB(255, 240, 240, 240),
@@ -11,7 +13,9 @@ final ThemeData lightTheme = ThemeData.light(useMaterial3: true).copyWith(
 
 final ThemeData darkTheme = ThemeData.dark(useMaterial3: true).copyWith(
   primaryColor: Colors.cyan[200],
-  textTheme: GoogleFonts.poppinsTextTheme(),
+   textTheme: Typography.material2018(platform: TargetPlatform.android).white.apply(
+    fontFamily: 'Poppins',
+  ),
   canvasColor: Colors.white,
   cardColor: const Color.fromARGB(255, 20, 20, 20),
   highlightColor: Colors.black,
